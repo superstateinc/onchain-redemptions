157359eb7446c5d94076e0af5029410bad0f53ff
Jon <pokemonmasterjmw@gmail.com>
2024-05-29 09:29:25 -0400

    commit progress to import oz

--- src/Redemption.sol
+++ src/Redemption.sol
@@ -1,16 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {AggregatorV3Interface} from "chainlink/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract Redemption {
     address public immutable USTB;
     AggregatorV3Interface public immutable USTB_ORACLE;
     address public immutable USDC;
 
-    constructor(address fraxGovernorOmega) {
-        = fraxGovernorOmega;
+    /// @notice Admin address with exclusive privileges for withdrawing tokens
+    address public immutable ADMIN;
+
+    constructor(address _ustb, address _ustbOracle, address _usdc) {
+        USTB = _ustb;
+        USTB_ORACLE = AggregatorV3Interface(_ustbOracle);
+        USDC = _usdc;
     }
+
+    // transfer in usdc
+    // function to withdraw usdc
 
 }
