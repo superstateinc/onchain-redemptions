0530c942465c91350fc3015b9c3edb829b13c50a
Jon <pokemonmasterjmw@gmail.com>
2024-05-29 11:37:38 -0400

    commit to install

--- src/Redemption.sol
+++ src/Redemption.sol
@@ -4,12 +4,28 @@ pragma solidity ^0.8.26;
 import {AggregatorV3Interface} from "chainlink/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "forge-std/console.sol";
 
+// Oracle integration borrowed from: https://github.com/FraxFinance/frax-oracles/blob/bd56532a3c33da95faed904a5810313deab5f13c/src/abstracts/ChainlinkOracleWithMaxDelay.sol
 contract Redemption {
     using SafeERC20 for IERC20;
 
-    address public immutable USTB;
-    AggregatorV3Interface public immutable USTB_ORACLE;
+    /// @notice The precision of the oracle
+    uint256 public constant ORACLE_PRECISION = 1e18;
+
+    /// @notice Chainlink aggregator
+    address public immutable CHAINLINK_FEED_ADDRESS;
+
+    /// @notice Decimals of ETH/USD chainlink feed
+    uint8 public immutable CHAINLINK_FEED_DECIMALS;
+
+    /// @notice Precision of ETH/USD chainlink feed
+    uint256 public immutable CHAINLINK_FEED_PRECISION;
+
+    uint256 public maximumOracleDelay;
+
+    IERC20 public immutable USTB;
+
     IERC20 public immutable USDC;
 
     /// @notice Admin address with exclusive privileges for withdrawing tokens
@@ -24,35 +40,106 @@ contract Redemption {
     /// @dev TODO
     error InsufficientBalance();
 
-    constructor(address _admin, address _ustb, address _ustbOracle, address _usdc) {
+    constructor(address _admin, address _ustb, address _ustbChainlinkFeedAddress, address _usdc) {
+        CHAINLINK_FEED_ADDRESS = _ustbChainlinkFeedAddress;
+        CHAINLINK_FEED_DECIMALS = AggregatorV3Interface(CHAINLINK_FEED_ADDRESS).decimals();
+        CHAINLINK_FEED_PRECISION = 10 ** uint256(CHAINLINK_FEED_DECIMALS);
+
+        maximumOracleDelay = 100000000000000000000; // TODO
         ADMIN = _admin;
-        USTB = _ustb;
-        USTB_ORACLE = AggregatorV3Interface(_ustbOracle);
+        USTB = IERC20(_ustb);
         USDC = IERC20(_usdc);
     }
 
     function _requireAuthorized() internal view {
-//        require(msg.sender != ADMIN, Unauthorized());
+        //        require(msg.sender == ADMIN, Unauthorized());
+        require(msg.sender == ADMIN, "Not admin"); // TODO
+    }
+
+    /// @notice The ```SetMaximumOracleDelay``` event is emitted when the max oracle delay is set
+    /// @param oldMaxOracleDelay The old max oracle delay
+    /// @param newMaxOracleDelay The new max oracle delay
+    event SetMaximumOracleDelay(uint256 oldMaxOracleDelay, uint256 newMaxOracleDelay);
+
+    /// @notice The ```_setMaximumOracleDelay``` function sets the max oracle delay to determine if Chainlink data is stale
+    /// @param _newMaxOracleDelay The new max oracle delay
+    function _setMaximumOracleDelay(uint256 _newMaxOracleDelay) internal {
+        emit SetMaximumOracleDelay({oldMaxOracleDelay: maximumOracleDelay, newMaxOracleDelay: _newMaxOracleDelay});
+        maximumOracleDelay = _newMaxOracleDelay;
+    }
+
+    function setMaximumOracleDelay(uint256 _newMaxOracleDelay) external {
+        _requireAuthorized();
+        _setMaximumOracleDelay(_newMaxOracleDelay);
+    }
+
+    function _getChainlinkPrice() internal view returns (bool _isBadData, uint256 _updatedAt, uint256 _price) {
+        (, int256 _answer,, uint256 _chainlinkUpdatedAt,) =
+            AggregatorV3Interface(CHAINLINK_FEED_ADDRESS).latestRoundData();
+
+        // If data is stale or negative, set bad data to true and return
+        _isBadData = _answer <= 0 || ((block.timestamp - _chainlinkUpdatedAt) > maximumOracleDelay);
+        _updatedAt = _chainlinkUpdatedAt;
+        _price = uint256(_answer);
+    }
+
+    /// @notice The ```getChainlinkPrice``` function returns the chainlink price and the timestamp of the last update
+    /// @dev Uses the same prevision as the chainlink feed, virtual so it can be overridden
+    /// @return _isBadData True if the data is stale or negative
+    /// @return _updatedAt The timestamp of the last update
+    /// @return _price The price
+    function getChainlinkPrice() external view returns (bool _isBadData, uint256 _updatedAt, uint256 _price) {
+        return _getChainlinkPrice();
+    }
+
+    /// @notice The ```getUstbPerUsdChainlink``` function returns USTB per USD using the Chainlink oracle
+    /// @return _isBadData If the Chainlink oracle is stale
+    /// @return _ustbPerUsd The Mkr per USD price
+    function getUstbPerUsdChainlink() public view returns (bool _isBadData, uint256 _ustbPerUsd) {
+        uint256 _usdPerUstbChainlinkRaw;
+        (_isBadData,, _usdPerUstbChainlinkRaw) = _getChainlinkPrice();
+        _ustbPerUsd = (ORACLE_PRECISION * CHAINLINK_FEED_PRECISION) / _usdPerUstbChainlinkRaw;
     }
 
     // user approves usdc in our ui, outside of this contract
     // user calls redeem function
     // redeem function takes ustb amount as arg, spends allowance of ustb, calcs usdc out amount, burns ustb
-    function redeem(uint256 amount) external {
-        (, int256 answer, uint256 startedAt, uint256 updatedAt,) =
-            USTB_ORACLE.latestRoundData();
+    function redeem(uint256 ustbRedemptionAmount) external {
+        (bool isBadData, uint256 ustbPerUsd) = getUstbPerUsdChainlink();
+
+        require(!isBadData, "TODO");
+
+        // TODO: check math in tests, it is probably wrong
+        uint256 usdcOut = (ustbRedemptionAmount * ustbPerUsd);
+
+        // TODO: probably dont need this
+        //        require(usdcOut <= USDC.balanceOf(address(this)), "TODO");
+
+        USTB.transferFrom(msg.sender, address(this), ustbRedemptionAmount);
+        USDC.transfer(msg.sender, usdcOut);
+
+        // TODO: generate and use USTB interface
+        bytes memory data = abi.encodeWithSignature("burn(uint256)", ustbRedemptionAmount);
+        (bool success,) = address(USTB).call(data);
+        require(success, "TODO");
+
+        //TODO: emit event
     }
 
     // transfer in usdc, outside of this contract
     function withdraw(address _token, address to, uint256 amount) external {
         _requireAuthorized();
-//        require(amount > 0, BadArgs());
+        //        require(amount > 0, BadArgs());
+        require(amount > 0, "TODO 1");
 
         IERC20 token = IERC20(_token);
         uint256 balance = token.balanceOf(address(this));
 
-//        require(balance >= amount, InsufficientBalance());
+        //        require(balance >= amount, InsufficientBalance());
+        require(balance >= amount, "Not enough balance"); //TODO
 
         token.safeTransfer(to, amount);
+
+        //TODO: emit event
     }
 }
--- test/Redemption.t.sol
+++ test/Redemption.t.sol
@@ -3,22 +3,89 @@ pragma solidity ^0.8.26;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Redemption} from "../src/Redemption.sol";
+import {Oracle} from "./Oracle.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract RedemptionTest is Test {
+    address public admin = address(this);
+
+    address constant allowList = 0x42d75C8FdBBF046DF0Fe1Ff388DA16fF99dE8149;
+    address allowListAdmin = 0x8C7Db8A96d39F76D9f456db23d591C2FDd0e2F8a;
+
+    IERC20 constant USTB = IERC20(0x43415eB6ff9DB7E26A15b704e7A3eDCe97d31C4e);
+    IERC20 constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    address constant _ustb_holder = 0xB8851D8fdd9a007A33f6b45BF602046644aBE81f;
+
+    uint256 constant usdc_amount = 10_000_000_000_000;
+    uint256 constant entity_id = 10_000_000_000_000;
+
+    Oracle public oracle;
     Redemption public redemption;
 
-    //    function setUp() public {
-    //        counter = new Redemption();
-    //        counter.setNumber(0);
-    //    }
-    //
-    //    function test_Increment() public {
-    //        counter.increment();
-    //        assertEq(counter.number(), 1);
-    //    }
-    //
-    //    function testFuzz_SetNumber(uint256 x) public {
-    //        counter.setNumber(x);
-    //        assertEq(counter.number(), x);
-    //    }
+    function setUp() public {
+        vm.createSelectFork(vm.envString("ETH_RPC_URL"), 19_976_215);
+
+        oracle = new Oracle();
+        redemption = new Redemption(admin, address(USTB), address(oracle), address(USDC));
+
+        // 10 million
+        deal(address(USDC), _ustb_holder, usdc_amount);
+
+        hoax(_ustb_holder);
+        USDC.transfer(address(redemption), usdc_amount);
+
+        assertGe(USDC.balanceOf(address(redemption)), 0);
+
+        vm.startPrank(allowListAdmin);
+        bytes memory data = abi.encodeWithSignature("setEntityIdForAddress(uint256,address)", entity_id, address(redemption));
+        (bool success,) = address(USTB).call(data);
+        require(success, "Setting perms failed");
+
+        bytes memory data2 = abi.encodeWithSignature("setIsAllowed(uint256,bool)", entity_id, true);
+        (bool success2,) = address(USTB).call(data2);
+        require(success2, "Setting perms 2 failed");
+
+        vm.stopPrank();
+    }
+
+    function testWithdraw() public {
+        hoax(admin);
+        redemption.withdraw(address(USDC), admin, usdc_amount);
+
+        assertEq(USDC.balanceOf(admin), usdc_amount);
+
+        hoax(admin);
+        vm.expectRevert(bytes("Not enough balance"));
+        redemption.withdraw(address(USDC), admin, 1);
+    }
+
+    function testWithdrawNotAdmin() public {
+        hoax(_ustb_holder);
+        vm.expectRevert(bytes("Not admin"));
+        redemption.withdraw(address(USDC), admin, 1);
+    }
+
+    function testWithdrawAmountZero() public {
+        hoax(admin);
+        vm.expectRevert(bytes("TODO 1"));
+        redemption.withdraw(address(USDC), admin, 0);
+    }
+
+    function testRedeem() public {
+        assertEq(USDC.balanceOf(_ustb_holder), 0);
+
+        uint256 ustbBalance = USTB.balanceOf(_ustb_holder);
+
+        vm.startPrank(_ustb_holder);
+        USDC.approve(address(redemption), ustbBalance);
+        redemption.redeem(ustbBalance);
+        vm.stopPrank();
+
+        assertEq(USTB.balanceOf(_ustb_holder), 0);
+        assertEq(USTB.balanceOf(address(redemption)), 0);
+
+        // TODO: assert USDC amount in _ustb_holder
+
+
+    }
 }
