01597825dd12c1bbc71e1ab455fcf062932df7b1
Jon <pokemonmasterjmw@gmail.com>
2024-10-17 13:13:20 -0400

    yuh

--- src/oracle/SuperstateOracle.sol
+++ src/oracle/SuperstateOracle.sol
@@ -10,12 +10,24 @@ import {Ownable2Step} from "openzeppelin-contracts/contracts/access/Ownable2Step
 /// @author Jon Walch (Superstate) https://github.com/superstateinc
 /// @notice A contract that allows Superstate to price USTB by extrapolating previous prices forward in real time
 contract SuperstateOracle is AggregatorV3Interface, Ownable2Step {
-    // TODO: notice explaining fields
+    /// @notice Represents a checkpoint for a Net Asset Value per Share (NAV/S) for a specific Superstate Business Day
     struct NavsCheckpoint {
+        /// @notice The timestamp of 5pm ET of the Superstate Business day for this NAV/S
         uint64 timestamp;
-        uint64 effective_at;
+        /// @notice The timestamp from which this NAV/S price can be used for realtime pricing
+        uint64 effectiveAt;
+        /// @notice The NAV/S at this checkpoint
         uint128 navs;
     }
+
+    /// @notice Number of days in seconds to keep extrapolating from latest checkpoint
+    uint256 public constant CHECKPOINT_EXPIRATION_PERIOD = 5 * 24 * 60 * 60; // 5 days in seconds
+
+    /// @notice Lowest acceptable Net Asset Value per Share price
+    uint256 public immutable MINIMUM_ACCEPTABLE_PRICE;
+
+    /// @notice The address of the USTB token proxy contract that this oracle prices
+    address public immutable USTB_TOKEN_PROXY_ADDRESS;
 
     /// @notice Decimals of SuperstateTokens
     uint8 public constant DECIMALS = 6;
@@ -23,39 +35,47 @@ contract SuperstateOracle is AggregatorV3Interface, Ownable2Step {
     /// @notice Version number of SuperstateOracle
     uint8 public constant VERSION = 1;
 
-    /// @notice Number of days in seconds to keep extrapolating from latest checkpoint
-    uint256 public constant LATEST_CHECKPOINT_GOOD_THROUGH = 5 * 24 * 60 * 60; // 5 days in seconds
-
-    /// @notice Lowest acceptable Net Asset Value per Share price
-    uint256 public immutable MINIMUM_ACCEPTABLE_PRICE;
-
     /// @notice Offchain Net Asset Value per Share checkpoints
     NavsCheckpoint[] public checkpoints;
 
-    // TODO: read only list token contract address?
-
     /// @notice The ```NewCheckpoint``` event is emitted when a new checkpoint is added
     /// @param timestamp The 5pm ET timestamp of when this price was calculated for offchain
-    /// @param effective_at When this checkpoint starts being used for pricing
+    /// @param effectiveAt When this checkpoint starts being used for pricing
     /// @param navs The Net Asset Value per Share (NAV/S) price (i.e. 10123456 is 10.123456)
-    event NewCheckpoint(uint64 timestamp, uint64 effective_at, uint128 navs);
+    event NewCheckpoint(uint64 timestamp, uint64 effectiveAt, uint128 navs);
 
-    /// @dev Thrown when an argument is invalid
-    error BadArgs();
-
-    /// @dev Thrown when there aren't at least 2 checkpoints where block.timestamp is after the effective_at timestamps for both
+    /// @dev Thrown when there aren't at least 2 checkpoints where block.timestamp is after the effectiveAt timestamps for both
     error CantGeneratePrice();
 
-    /// @dev Thrown when the latest checkpoint is too stale to use to price
-    error StaleCheckpoint();
+    /// @dev Thrown when the effectiveAt argument is invalid
+    error EffectiveAtInvalid();
+
+    /// @dev Thrown when the effectiveAt argument is not chronologically valid
+    error EffectiveAtNotChronological();
+
+    /// @dev Thrown when there is an effectiveAt in the future for a previously written checkpoint
+    error ExistingPendingEffectiveAt();
+
+    /// @dev Thrown when the navs argument is invalid
+    error NetAssetValuePerShareInvalid();
 
     /// @dev Thrown when the function is not implemented
     error NotImplemented();
 
-    constructor(address initialOwner) Ownable(initialOwner) {
+    /// @dev Thrown when the latest checkpoint is too stale to use to price
+    error StaleCheckpoint();
+
+    /// @dev Thrown when the timestamp argument is invalid
+    error TimestampInvalid();
+
+    /// @dev Thrown when the timestamp argument is chronologically invalid
+    error TimestampNotChronological();
+
+    constructor(address initialOwner, address ustbTokenProxy) Ownable(initialOwner) {
         // SUPERSTATE_TOKEN starts at $10.000000. An Oracle with 6 decimals would represent as 10_000_000.
         // This math will give us 7_000_000 or $7.000000.
         MINIMUM_ACCEPTABLE_PRICE = 7 * (10 ** uint256(DECIMALS));
+        USTB_TOKEN_PROXY_ADDRESS = ustbTokenProxy;
     }
 
     function decimals() external pure override returns (uint8) {
@@ -70,86 +90,111 @@ contract SuperstateOracle is AggregatorV3Interface, Ownable2Step {
         return VERSION;
     }
 
-    function _addCheckpoint(uint64 timestamp, uint64 effective_at, uint128 navs, bool override_effective_at) internal {
+    /// @notice Adds a new NAV/S checkpoint
+    /// @dev This function performs various checks to ensure the validity of the new checkpoint
+    /// @param timestamp The timestamp of the checkpoint (should be 5pm ET of the business day)
+    /// @param effectiveAt The time from which this checkpoint becomes effective (must be now or in the future)
+    /// @param navs The Net Asset Value per Share for this checkpoint
+    /// @param shouldOverrideEffectiveAt Flag to allow overriding an existing pending effective timestamp
+    function _addCheckpoint(uint64 timestamp, uint64 effectiveAt, uint128 navs, bool shouldOverrideEffectiveAt)
+        internal
+    {
         uint256 nowTimestamp = block.timestamp;
 
         // timestamp should refer to 5pm ET of a previous business day
-        if (timestamp >= nowTimestamp) revert BadArgs();
+        if (timestamp >= nowTimestamp) revert TimestampInvalid();
 
-        // effective_at must be now or in the future
-        if (effective_at < nowTimestamp) revert BadArgs();
+        // effectiveAt must be now or in the future
+        if (effectiveAt < nowTimestamp) revert EffectiveAtInvalid();
 
-        if (navs < MINIMUM_ACCEPTABLE_PRICE) revert BadArgs();
+        if (navs < MINIMUM_ACCEPTABLE_PRICE) revert NetAssetValuePerShareInvalid();
 
         // Can only add new checkpoints going chronologically forward
         if (checkpoints.length > 0) {
             NavsCheckpoint memory latest = checkpoints[checkpoints.length - 1];
 
-            if (latest.timestamp >= timestamp || latest.effective_at >= effective_at) {
-                revert BadArgs();
+            if (latest.timestamp >= timestamp) {
+                revert TimestampNotChronological();
+            }
+
+            if (latest.effectiveAt >= effectiveAt) {
+                revert EffectiveAtNotChronological();
             }
         }
 
-        // Revert if there is already a checkpoint with an effective_at in the future, unless override
+        // Revert if there is already a checkpoint with an effectiveAt in the future, unless override
         // Only start the check after 2 checkpoints, since two are needed to get a price at all
-        if (checkpoints.length > 1 && checkpoints[checkpoints.length - 1].effective_at > nowTimestamp) {
-            if (!override_effective_at) {
-                revert BadArgs();
+        if (checkpoints.length > 1 && checkpoints[checkpoints.length - 1].effectiveAt > nowTimestamp) {
+            if (!shouldOverrideEffectiveAt) {
+                revert ExistingPendingEffectiveAt();
             }
         }
 
-        checkpoints.push(NavsCheckpoint({timestamp: timestamp, effective_at: effective_at, navs: navs}));
+        checkpoints.push(NavsCheckpoint({timestamp: timestamp, effectiveAt: effectiveAt, navs: navs}));
 
-        emit NewCheckpoint({timestamp: timestamp, effective_at: effective_at, navs: navs});
+        emit NewCheckpoint({timestamp: timestamp, effectiveAt: effectiveAt, navs: navs});
     }
 
-    // TODO: notice, automated systems only use this
-    function addCheckpoint(uint64 timestamp, uint64 effective_at, uint128 navs, bool override_effective_at) external {
+    /// @notice Adds a single NAV/S checkpoint
+    /// @dev This function can only be called by the contract owner. Automated systems should only use this and not `addCheckpoints`
+    /// @param timestamp The timestamp of the checkpoint
+    /// @param effectiveAt The time from which this checkpoint becomes effective
+    /// @param navs The Net Asset Value per Share for this checkpoint
+    /// @param shouldOverrideEffectiveAt Flag to allow overriding an existing pending effective timestamp
+    function addCheckpoint(uint64 timestamp, uint64 effectiveAt, uint128 navs, bool shouldOverrideEffectiveAt)
+        external
+    {
         _checkOwner();
 
         _addCheckpoint({
             timestamp: timestamp,
-            effective_at: effective_at,
+            effectiveAt: effectiveAt,
             navs: navs,
-            override_effective_at: override_effective_at
+            shouldOverrideEffectiveAt: shouldOverrideEffectiveAt
         });
     }
 
-    // TODO: notice, only manually use this
+    /// @notice Adds multiple NAV/S checkpoints at once
+    /// @dev This function can only be called by the contract owner. Should not be used via automated systems.
+    /// @param _checkpoints An array of NavsCheckpoint structs to be added
     function addCheckpoints(NavsCheckpoint[] calldata _checkpoints) external {
         _checkOwner();
 
         for (uint256 i = 0; i < _checkpoints.length; ++i) {
             _addCheckpoint({
                 timestamp: _checkpoints[i].timestamp,
-                effective_at: _checkpoints[i].effective_at,
+                effectiveAt: _checkpoints[i].effectiveAt,
                 navs: _checkpoints[i].navs,
-                override_effective_at: true
+                shouldOverrideEffectiveAt: true
             });
         }
     }
 
-    // TODO: notice
-    // TODO: public?
-    function calculate_realtime_navs(
-        uint128 unix_timestamp_to_price,
-        uint128 early_navs,
-        uint128 early_timestamp,
-        uint128 later_navs,
-        uint128 later_timestamp
-    ) internal pure returns (uint128 answer) {
-        answer = later_navs
-            + ((later_navs - early_navs) * (unix_timestamp_to_price - later_timestamp))
-                / (later_timestamp - early_timestamp);
+    /// @notice Calculates the real-time NAV based on two checkpoints
+    /// @dev Uses linear interpolation to estimate the NAV/S at the target timestamp
+    /// @param targetTimestamp The timestamp for which to calculate the NAV/S
+    /// @param earlierCheckpointNavs The NAV/S of the earlier checkpoint
+    /// @param earlierCheckpointTimestamp The timestamp of the earlier checkpoint
+    /// @param laterCheckpointNavs The NAV/S of the later checkpoint
+    /// @param laterCheckpointTimestamp The timestamp of the later checkpoint
+    /// @return answer The calculated real-time NAV/S
+    function calculateRealtimeNavs(
+        uint128 targetTimestamp,
+        uint128 earlierCheckpointNavs,
+        uint128 earlierCheckpointTimestamp,
+        uint128 laterCheckpointNavs,
+        uint128 laterCheckpointTimestamp
+    ) public pure returns (uint128 answer) {
+        answer = laterCheckpointNavs
+            + ((laterCheckpointNavs - earlierCheckpointNavs) * (targetTimestamp - laterCheckpointTimestamp))
+                / (laterCheckpointTimestamp - earlierCheckpointTimestamp);
     }
 
-    // TODO: notice
-    function getRoundData(uint80)
-        external
-        pure
-        override
-        returns (uint80, int256, uint256, uint256, uint80)
-    {
+    /// @notice Placeholder function to comply with the Chainlink AggregatorV3Interface
+    /// @dev This function is not implemented and will always revert
+    /// @dev param: roundId The round ID (unused)
+    /// @return Tuple of round data (never actually returned)
+    function getRoundData(uint80) external pure override returns (uint80, int256, uint256, uint256, uint80) {
         revert NotImplemented();
     }
 
@@ -157,6 +202,14 @@ contract SuperstateOracle is AggregatorV3Interface, Ownable2Step {
     // will give different prices for the same _roundId based on the block.timestamp
     // startedAt and updatedAt give the timestamp of the price
     // only gives latest price
+
+    /// @notice Retrieves the latest NAV/S real-time price data
+    /// @dev Calculates the current NAV/S based on the two most recent valid checkpoints
+    /// @return roundId The ID of the latest round
+    /// @return answer The current NAV/S
+    /// @return startedAt The timestamp when this round started (current block timestamp)
+    /// @return updatedAt The timestamp of the last update (current block timestamp, won't ever update)
+    /// @return answeredInRound The round ID in which the answer was computed
     function latestRoundData()
         external
         view
@@ -169,28 +222,28 @@ contract SuperstateOracle is AggregatorV3Interface, Ownable2Step {
         uint128 nowTimestamp = uint128(block.timestamp);
 
         // We will only have one checkpoint that isn't effective yet the vast majority of the time
-        while (latestIndex != 0 && checkpoints[latestIndex].effective_at > nowTimestamp) {
+        while (latestIndex != 0 && checkpoints[latestIndex].effectiveAt > nowTimestamp) {
             latestIndex -= 1;
+            if (latestIndex == 0) revert CantGeneratePrice(); // need at least two rounds i.e. 0 and 1
         }
 
-        if (latestIndex == 0) revert CantGeneratePrice(); // need at least two rounds i.e. 0 and 1
-        NavsCheckpoint memory later = checkpoints[latestIndex];
-        NavsCheckpoint memory earlier = checkpoints[latestIndex - 1];
+        NavsCheckpoint memory laterCheckpoint = checkpoints[latestIndex];
+        NavsCheckpoint memory earlierCheckpoint = checkpoints[latestIndex - 1];
 
-        if (nowTimestamp > later.effective_at + LATEST_CHECKPOINT_GOOD_THROUGH) {
+        if (nowTimestamp > laterCheckpoint.effectiveAt + CHECKPOINT_EXPIRATION_PERIOD) {
             revert StaleCheckpoint();
         }
 
-        uint128 realtime_navs = calculate_realtime_navs({
-            unix_timestamp_to_price: nowTimestamp,
-            early_navs: earlier.navs,
-            early_timestamp: earlier.timestamp,
-            later_navs: later.navs,
-            later_timestamp: later.timestamp
+        uint128 realtimeNavs = calculateRealtimeNavs({
+            targetTimestamp: nowTimestamp,
+            earlierCheckpointNavs: earlierCheckpoint.navs,
+            earlierCheckpointTimestamp: earlierCheckpoint.timestamp,
+            laterCheckpointNavs: laterCheckpoint.navs,
+            laterCheckpointTimestamp: laterCheckpoint.timestamp
         });
 
         roundId = uint80(latestIndex);
-        answer = int256(uint256(realtime_navs));
+        answer = int256(uint256(realtimeNavs));
         startedAt = nowTimestamp;
         updatedAt = nowTimestamp;
         answeredInRound = uint80(latestIndex);
