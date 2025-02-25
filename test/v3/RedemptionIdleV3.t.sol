6b78d13d0f96f1d2feb9c8f7cdddb0bd46308bf5
khalil <khalil@superstate.co>
2025-02-25 13:57:39 -0500

    implement testRedeemBadDataOldDataFail override

--- test/v3/RedemptionIdleV3.t.sol
+++ test/v3/RedemptionIdleV3.t.sol
@@ -94,7 +94,18 @@ contract RedemptionIdleTestV3 is RedemptionIdleTestV2 {
     }
 
     function testRedeemBadDataOldDataFail() public override {
+        vm.warp(block.timestamp + 5 days + 1);
 
+        assertEq(USDC.balanceOf(SUPERSTATE_TOKEN_HOLDER), 0);
+
+        vm.expectRevert(SuperstateOracle.StaleCheckpoint.selector);
+        redemption.maxUstbRedemptionAmount();
+
+        vm.startPrank(SUPERSTATE_TOKEN_HOLDER);
+        SUPERSTATE_TOKEN.approve(address(redemption), 100);
+        vm.expectRevert(SuperstateOracle.StaleCheckpoint.selector);
+        redemptionV3.redeem(SUPERSTATE_REDEMPTION_RECEIVER, 100);
+        vm.stopPrank();
     }
 
     function testRedeemFuzz(uint256 superstateTokenRedeemAmount) public override {
